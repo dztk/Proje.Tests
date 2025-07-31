f5452d41844c11029002f4a7cfd73466a7d9c825
dztk <100531246+dztk@users.noreply.github.com>
2025-07-31 23:35:31 +0300

    Add files via upload

---
+++ StokServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Proje.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Proje.Tests
+{
+    public class StokServiceTests
+    {
+        [Fact]
+        public void StokDusur_DogruAzaltiyorMu()
+        {
+            // Arrange
+            var servis = new StokService();
+
+            // Act
+            int sonuc = servis.StokDusur(10, 3);
+
+            // Assert
+            Assert.Equal(7, sonuc); 
+        }
+    }
+}
+
+
+
+
